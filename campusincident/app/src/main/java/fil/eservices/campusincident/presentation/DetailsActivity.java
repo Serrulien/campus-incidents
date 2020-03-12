121b53bdab222bed88a69deec3c9dba4800aee23
yahiatene <virus385625>
2020-03-12 10:54:20 +0100

    Feature : Add button wish resolve of incident

--- campusincident/app/src/main/java/fil/eservices/campusincident/presentation/DetailsActivity.java
+++ campusincident/app/src/main/java/fil/eservices/campusincident/presentation/DetailsActivity.java
@@ -3,6 +3,7 @@ package fil.eservices.campusincident.presentation;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -14,6 +15,10 @@ import fil.eservices.campusincident.data.model.Incident;
 public class DetailsActivity extends AppCompatActivity {
 
     private ImageButton backButton;
+    private Button resolveButton;
+    private TextView numberResolve;
+    private String textViewValue;
+    private int number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,8 @@ public class DetailsActivity extends AppCompatActivity {
         Incident incident = (Incident) getIntent().getSerializableExtra("incident");
 
         setContentView(R.layout.activity_details);
+
+        numberResolve = findViewById(R.id.nombre_evaluation);
 
         TextView title = findViewById(R.id.incident_name);
         title.setText(incident.getTitle());
@@ -42,6 +49,7 @@ public class DetailsActivity extends AppCompatActivity {
         TextView catTV = findViewById(R.id.incident_category);
         catTV.setText(categories.toString());
 
+        setBtnCreate();
         backButton();
     }
 
@@ -54,6 +62,24 @@ public class DetailsActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+    }
+
+    /**
+     * To set on click on wish resolve incident
+     */
+    private void setBtnCreate(){
+        resolveButton = findViewById(R.id.btn_souhaiter_resolution);
+        textViewValue = numberResolve.getText().toString();
+        number = Integer.parseInt(textViewValue);
+        resolveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                number++;
+                textViewValue = String.valueOf(number);
+                numberResolve.setText(textViewValue);
+                resolveButton.setEnabled(false);
             }
         });
     }
